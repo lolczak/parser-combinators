910b4c48bdeec85cb51e2694c7ace61cc41bd07e
lolczak <lukasz.olczak@merchenta.com>
2014-11-15 22:34:08 +0100

    addition with ? separated

--- src/main/scala/org/lolczak/parser/ArithmeticExpressionParser.scala
+++ src/main/scala/org/lolczak/parser/ArithmeticExpressionParser.scala
@@ -4,12 +4,28 @@ import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
 object ArithmeticExpressionParser extends StandardTokenParsers {
 
-  def parse(question: String): ParseResult[Expression] = ???
+//  lexical.delimiters ++= List("What", "is", "plus", "?")
+  lexical.delimiters ++= List("-","?")
+  lexical.reserved ++= List("What", "is", "plus")
+
+  def factor: Parser[Int] = ("-" ~> numericLit^^( - _.toInt) )|(numericLit ^^ (_.toInt))
+
+  def leftFactor: Parser[Int] = "What" ~ "is" ~> factor <~ "plus"
+
+  def rightFactor: Parser[Int] = factor <~ "?"
+
+  def expr: Parser[Expression] = leftFactor ~ rightFactor ^^ { case a ~ b => Addition(a, b)}
+
+  def parse(question: String): ParseResult[Expression] = expr(new lexical.Scanner(question))
 
 }
 
 sealed trait Expression
+
 case class Addition(arg1: Int, arg2: Int) extends Expression
+
 case class Subtraction(arg1: Int, arg2: Int) extends Expression
+
 case class Multiplication(arg1: Int, arg2: Int) extends Expression
+
 case class Division(arg1: Int, arg2: Int) extends Expression
--- src/test/scala/org/lolczak/parser/ArithmeticExpressionParserSpec.scala
+++ src/test/scala/org/lolczak/parser/ArithmeticExpressionParserSpec.scala
@@ -5,45 +5,42 @@ import org.scalacheck.{Gen, Shrink}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.JavaConversions._
+
 object ArithmeticExpressionGenerators {
 
   val Factor = Gen.choose(Int.MinValue, Int.MaxValue)
 
-//  val Whitespace = Gen.listOf(Gen.oneOf(" ", "\t", "\r")).map(_.mkString)
+  //  val Whitespace = Gen.listOf(Gen.oneOf(" ", "\t", "\r")).map(_.mkString)
 
-    val Whitespace = Gen.oneOf(" ", "\t")
+  val Whitespace = Gen.oneOf(" ", "  ", "\t", " \t")
 
-  val Preamble = Gen.const("What is")
+  val Preamble = Gen.sequence(Seq(Whitespace, Gen.const("What is"), Whitespace)).map(_.mkString)
 
-  val Postamble = Gen.const("?")
+  val Postamble = Gen.sequence(Seq(Whitespace, Gen.const("?"), Whitespace)).map(_.mkString)
 
-  val Plus = Gen.const("plus")
+  val Plus = Gen.sequence(Seq(Whitespace, Gen.const("plus"), Whitespace)).map(_.mkString)
 
   val AdditionGenerator =
     for {
-      w1 <- Whitespace
-      w2 <- Whitespace
-      w3 <- Whitespace
-      w4 <- Whitespace
-      w5 <- Whitespace
+      preamble <- Preamble
+      plus <- Plus
+      postamble <- Postamble
       f1 <- Factor
       f2 <- Factor
-    } yield (w1 + "What is" + w2 + f1 + w3 + "plus" + w4 + f2 + "?" + w5, f1, f2)
-  //    Gen.sequence(Seq(Whitespace, Preamble, Whitespace, Factor, Whitespace, Plus, Whitespace, Factor, Whitespace, Postamble, Whitespace))
-  //      .map(_.mkString)
+    } yield (preamble + f1 + plus + f2 + postamble, f1, f2)
 
 }
 
 class ArithmeticExpressionParserSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
 
-    implicit override val generatorDrivenConfig =
-      PropertyCheckConfig(minSize = 2, maxSize = 200)
-
   "An ArithmeticExpressionParser" should "parse addition expression" in {
     implicit val noShrink = Shrink[String](x => Stream.empty)
-    forAll(AdditionGenerator) { (question) =>
-      println(question._1)
-      ArithmeticExpressionParser.parse(question._1) should equal(ArithmeticExpressionParser.Success)
+    forAll(AdditionGenerator) { (testCase) =>
+      val (question, f1, f2) = testCase
+      val result = ArithmeticExpressionParser.parse(question)
+      result should be('successful)
+      result.get should equal(Addition(f1, f2))
     }
   }
 
