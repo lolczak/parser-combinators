e28f02d1b4abdf1b70436860771ff9c2252e4a44
lolczak <lukasz.olczak@merchenta.com>
2014-11-15 22:42:49 +0100

    bad data test

--- src/test/scala/org/lolczak/parser/ArithmeticExpressionParserSpec.scala
+++ src/test/scala/org/lolczak/parser/ArithmeticExpressionParserSpec.scala
@@ -9,6 +9,8 @@ import scala.collection.JavaConversions._
 
 object ArithmeticExpressionGenerators {
 
+  val genStringStream = Gen.containerOf[Stream,String](Gen.alphaStr)
+
   val Factor = Gen.choose(Int.MinValue, Int.MaxValue)
 
   //  val Whitespace = Gen.listOf(Gen.oneOf(" ", "\t", "\r")).map(_.mkString)
@@ -17,7 +19,7 @@ object ArithmeticExpressionGenerators {
 
   val Preamble = Gen.sequence(Seq(Whitespace, Gen.const("What is"), Whitespace)).map(_.mkString)
 
-  val Postamble = Gen.sequence(Seq(Whitespace, Gen.const("?"), Whitespace)).map(_.mkString)
+  val Postamble = Gen.sequence(Seq(Gen.const("?"), Whitespace)).map(_.mkString)
 
   val Plus = Gen.sequence(Seq(Whitespace, Gen.const("plus"), Whitespace)).map(_.mkString)
 
@@ -39,8 +41,17 @@ class ArithmeticExpressionParserSpec extends FlatSpec with Matchers with Generat
     forAll(AdditionGenerator) { (testCase) =>
       val (question, f1, f2) = testCase
       val result = ArithmeticExpressionParser.parse(question)
+      println(s"q: $question, r:$result")
       result should be('successful)
       result.get should equal(Addition(f1, f2))
+    }
+  }
+
+  it should "report an error when question doesn't meet grammae" in {
+    forAll(Gen.alphaStr) { question =>
+      val result = ArithmeticExpressionParser.parse(question)
+      println(s"q: $question, r:$result")
+      result shouldNot be('successful)
     }
   }
 
