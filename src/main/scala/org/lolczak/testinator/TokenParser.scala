2e371a0397b027bf14350d39ae11a8c7e7f4f702
lolczak <lukasz.olczak@merchenta.com>
2014-11-17 23:01:19 +0100

    token parser

--- src/main/scala/org/lolczak/testinator/TokenParser.scala
+++ src/main/scala/org/lolczak/testinator/TokenParser.scala
@@ -2,4 +2,11 @@ package org.lolczak.testinator
 
 object TokenParser {
 
+  val pattern = """\s*Hi\s+\S+\s*Your\s+token\s+is\s*:\s*(\S+)\s*""".r
+
+  def parse(tokenMsg: String): Either[String, String] = tokenMsg match {
+    case pattern(token) => Right(token)
+    case _ => Left(s"Token message incorrect: [$tokenMsg]")
+  }
+
 }
--- src/test/scala/org/lolczak/testinator/TokenParserSpec.scala
+++ src/test/scala/org/lolczak/testinator/TokenParserSpec.scala
@@ -1,7 +1,42 @@
 package org.lolczak.testinator
 
-import org.scalatest.FunSuite
+import org.scalacheck.{Gen, Shrink}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
 
-class TokenParserSpec extends FunSuite {
+import scala.collection.JavaConversions._
+
+object TokenParserSpec {
+
+  val whitespaceGen = Gen.nonEmptyListOf(Gen.oneOf(" ", "\t")).map(_.mkString)
+
+  val messageGen = for {
+    preamble <- Gen.sequence(Seq(whitespaceGen, Gen.const("Hi"), whitespaceGen)).map(_.mkString)
+    name <- Gen.alphaStr
+    middle <- Gen.sequence(Seq(whitespaceGen, Gen.const("Your token is:"), whitespaceGen)).map(_.mkString)
+    whitespace <- whitespaceGen
+    name <- Gen.alphaStr.suchThat(_.nonEmpty)
+    token <- Gen.alphaStr.suchThat(_.nonEmpty)
+    msg = preamble + name + "." + middle + whitespace + token
+  } yield (msg, token)
+
+}
+
+class TokenParserSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
+
+  import org.lolczak.testinator.TokenParserSpec._
+
+  "A TokenParser" should "parse token message" in {
+    forAll(messageGen) { case (msg, token) =>
+      TokenParser.parse(msg) should equal(Right(token))
+    }
+  }
+
+  it should "report an error when message doesn't meet grammar" in {
+    forAll(Gen.alphaStr) { question =>
+      TokenParser.parse(question) should matchPattern { case Left(_) =>}
+    }
+  }
+
 
 }
