3fdab1dc529766c3b7da15a81a9323426aa8beb1
lolczak <lukasz.olczak@merchenta.com>
2014-11-17 21:34:32 +0100

    working pretty streamable solution

--- src/main/scala/org/lolczak/QuizApp.scala
+++ src/main/scala/org/lolczak/QuizApp.scala
@@ -3,7 +3,9 @@ package org.lolczak
 import io.shaka.http.Http._
 import io.shaka.http.Request.GET
 import org.lolczak.parser._
+
 import scalaz.effect.IO
+import scalaz.stream._
 
 object QuizApp extends App {
 
@@ -11,37 +13,54 @@ object QuizApp extends App {
 
   val pre = "Hi lukasz. Your token is: "
 
-  val token = http(GET(s"http://testinator-project.appspot.com/startTest/lukasz")).entityAsString.substring(pre.length)
-println(s"token: $token")
+  val end = "You have finished"
 
-  val question:IO[Expression] = IO {
+  val token = http(GET(s"http://testinator-project.appspot.com/startTest/lukasz")).entityAsString.substring(pre.length)
+  println(s"token: $token")
+
+  val question: IO[String] = IO {
     val get = http(GET(s"http://testinator-project.appspot.com/$token/nextQuestion")).entityAsString
     println(s"got question: $get")
-    ArithmeticExpressionParser.parse(get).get
+    get
   }
 
-  def eval(expr:Expression): Int = expr match {
-    case Addition(x,y) => x+y
-    case Subtraction(x,y) => x-y
-    case Multiplication(x,y) => x*y
+  def eval(question: String): Int = {
+    val expr = ArithmeticExpressionParser.parse(question).get
+
+    expr match {
+      case Addition(x, y) => x + y
+      case Subtraction(x, y) => x - y
+      case Multiplication(x, y) => x * y
+    }
   }
 
-  val answer: IO[Int => Boolean] = IO { result =>
+  val answer: Int => IO[ Boolean] = {result => IO {
     println(s"responding $result")
     val get = http(GET(s"http://testinator-project.appspot.com/$token/answer/$result")).entityAsString
     println(s"got result: $get")
     get.trim == "pass"
-  }
+  }}
 
-  val result = QuizRunner.playIo(9, eval)(question, answer)
+ val questions= Process.repeatEval(question).takeWhile(x=> !x.contains(end))
+
+  /**
+   * An effectful channel, to which we can send values and
+   * get back responses. Modeled as a source of effectful
+   * functions.
+   */
+  //    type Channel[+F[_],-I,O] = Process[F, I => F[O]]
+
+  //Channel[F,B, Boolean] Process[IO, Int=>IO[Boolan]
+
+  val answers = Process.repeatEval(IO{answer})
+
+  val result = QuizRunner.play[IO, String, Int](eval)(questions)(answers)
 
   val won = result.unsafePerformIO()
 
   println(http(GET(s"http://testinator-project.appspot.com/$token/nextQuestion")).entityAsString)
 
   println(s"won: $won")
-
-
 
 
 }
--- src/main/scala/org/lolczak/QuizRunner.scala
+++ src/main/scala/org/lolczak/QuizRunner.scala
@@ -1,6 +1,8 @@
 package org.lolczak
 
+import scalaz.{Catchable, Monad}
 import scalaz.effect.IO
+import scalaz.stream._
 
 object QuizRunner {
 
@@ -11,7 +13,22 @@ object QuizRunner {
       question <- questions
       verifier <- verifiers
       turn = playOneTurn(answerEval)(question, verifier)
-      result <- if (no > 0 && turn) playIo(no - 1, answerEval)(questions, verifiers) else IO { turn }
+      result <- if (no > 0 && turn) playIo(no - 1, answerEval)(questions, verifiers)
+      else IO {
+        turn
+      }
     } yield result
+
+
+  def play[F[_], A, B](answerOn: A => B)(questions: Process[F, A])(verifiers: Channel[F,B, Boolean])(implicit F: Monad[F], C: Catchable[F]):F[Boolean] = {
+    val answers = questions.map(answerOn)
+
+
+    val results = answers.through(verifiers)
+
+    val result = results.takeWhile(x=>x)
+
+    result.runLastOr[F, Boolean](true)
+  }
 
 }
