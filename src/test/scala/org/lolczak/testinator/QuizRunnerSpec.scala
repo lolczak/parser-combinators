bc996fbfc0411dd39075198a38dfef08461f7d6f
lolczak <lukasz.olczak@merchenta.com>
2014-11-18 21:32:01 +0100

    refactoring

--- src/test/scala/org/lolczak/testinator/QuizRunnerSpec.scala
+++ src/test/scala/org/lolczak/testinator/QuizRunnerSpec.scala
@@ -9,57 +9,67 @@ import scalaz.stream._
 
 class QuizRunnerSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
 
-  import QuestionGenerators._
+  import org.lolczak.testinator.QuestionGenerators._
 
   implicit val noShrink = Shrink[String](x => Stream.empty)
 
-  def toVerifiers = { correct:Int => answer:Int => Task delay { correct == answer } }
+  def toVerifiers = { correct: Int => answer: Int => Task delay { correct == answer } }
+
+  def unzip(questionsWithAnswers: List[(String, Int)]): (Process[Task, String], Process[Task, Int => Task[Boolean]]) = {
+    val questions: Process[Task, String] = Process.emitAll(questionsWithAnswers.unzip._1)
+    val answers: Process[Task, Int] = Process.emitAll(questionsWithAnswers.unzip._2)
+    val verifiers = answers map toVerifiers
+    (questions, verifiers)
+  }
 
   "A QuizRunner" should "play until end of question stream" in {
     var counter = 0
-    val questions: Process[Task,Int] = Process.repeatEval(Task.delay{
+    val questions: Process[Task, Int] = Process.repeatEval(Task.delay {
       val ret = counter
       counter = counter + 1
       ret
     }).take(10)
-    val verifiers = Process.range(0,10) map { correct=> { toCheck:Int=>
+    val verifiers = Process.range(0, 10) map { correct => { toCheck: Int =>
       println(s"compering $toCheck vs $correct")
-      Task.delay( toCheck==correct )}}
+      Task.delay(toCheck == correct)
+    }
+    }
 
-    val result = QuizRunner.play({x:Int=> x})(questions)(verifiers)
+    val result = QuizRunner.play({ x: Int => x})(questions)(verifiers)
     result.run should be(true)
     counter should be(10)
   }
 
   it should "return true if all answers were correct" in {
     forAll(questionWithAnswerGen) { questionsWithAnswers =>
-      val questions : Process[Task, String] = Process.emitAll(questionsWithAnswers.unzip._1)
-      val answers : Process[Task, Int] = Process.emitAll(questionsWithAnswers.unzip._2)
-      val verifiers = answers map toVerifiers
-
+      val (questions, verifiers) = unzip(questionsWithAnswers)
       val result = QuizRunner.run(questions, verifiers)
       result.run should be(true)
     }
   }
 
   it should "return false if any answer was incorrect" in {
-    val questions: Process[Task,Int] = Process.range(0,10)
-    val verifiers = Process.range(0,10) map { correct=> { toCheck:Int=>
-      Task.delay( correct < 5 )}}
-    val result = QuizRunner.play({x:Int=> x})(questions)(verifiers)
+    val questions: Process[Task, Int] = Process.range(0, 10)
+    val verifiers = Process.range(0, 10) map { correct => { toCheck: Int =>
+      Task.delay(correct < 5)
+    }
+    }
+    val result = QuizRunner.play({ x: Int => x})(questions)(verifiers)
     result.run should be(false)
   }
 
   it should "stop playing on wrong answer" in {
     var counter = 0
-    val questions: Process[Task,Int] = Process.repeatEval(Task.delay{
+    val questions: Process[Task, Int] = Process.repeatEval(Task.delay {
       val ret = counter
       counter = counter + 1
       ret
     })
-    val verifiers = Process.range(0,10) map { correct=> { toCheck:Int=>
-      Task.delay( correct < 5 )}}
-    val result = QuizRunner.play({x:Int=> x})(questions)(verifiers)
+    val verifiers = Process.range(0, 10) map { correct => { toCheck: Int =>
+      Task.delay(correct < 5)
+    }
+    }
+    val result = QuizRunner.play({ x: Int => x})(questions)(verifiers)
     result.run
     counter should be(6)
   }
